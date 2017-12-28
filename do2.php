806ad8cadfc75dc7ce325e787310c7d512b9c674
jsy135135 <732677288@qq.com>
2017-12-28 17:20:56 +0800

    开发了222功能 Signed-off-by: jsy135135 <732677288@qq.com>

--- do2.php
+++ do2.php
@@ -5,10 +5,11 @@
  * @email:732677288@qq.com
  * @Date:   2017-12-28 15:30:08
  * @Last Modified by:   jsy135135
- * @Last Modified time: 2017-12-28 17:19:27
+ * @Last Modified time: 2017-12-28 17:20:44
  */
 require './wechat.class.php';
 header('Content-Type:text/html;charset=utf-8');
 $wechat = new Wechat();
 $wechat->getUserInfo($_GET['openid']);
 echo 111;
+echo 222;
