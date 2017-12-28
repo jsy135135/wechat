10838992f68e718519420470958afe380862b51e
jsy135135 <732677288@qq.com>
2017-12-28 17:20:31 +0800

    开发了11功能，修复了自动吐钱的bug Signed-off-by: jsy135135 <732677288@qq.com>

--- do2.php
+++ do2.php
@@ -5,9 +5,10 @@
  * @email:732677288@qq.com
  * @Date:   2017-12-28 15:30:08
  * @Last Modified by:   jsy135135
- * @Last Modified time: 2017-12-28 15:33:09
+ * @Last Modified time: 2017-12-28 17:19:27
  */
 require './wechat.class.php';
 header('Content-Type:text/html;charset=utf-8');
 $wechat = new Wechat();
 $wechat->getUserInfo($_GET['openid']);
+echo 111;
